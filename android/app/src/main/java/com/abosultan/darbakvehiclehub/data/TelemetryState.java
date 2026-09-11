fed0718fb62d3668a10fc18449438c07aa2dfec6
abo-sultann <abo-sultann@hotmail.com>
2026-09-11 12:17:34 +0300

    Add normalized telemetry state model

---
+++ android/app/src/main/java/com/abosultan/darbakvehiclehub/data/TelemetryState.java
@@ -0,0 +1,12 @@
+package com.abosultan.darbakvehiclehub.data;
+
+public final class TelemetryState {
+    public float flPsi = Float.NaN;
+    public float frPsi = Float.NaN;
+    public float rlPsi = Float.NaN;
+    public float rrPsi = Float.NaN;
+    public int speedKph = -1;
+    public int rpm = -1;
+    public int coolantC = Integer.MIN_VALUE;
+    public float voltage = Float.NaN;
+}
